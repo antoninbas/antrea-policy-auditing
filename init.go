be3ff435a1bbbefb99feaa203018ef23ca56f5e5
Stan Wong <swong394@gmail.com>
2021-06-08 23:48:45 -0400

    set up go-client and read in initial netpol state

---
+++ init.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"fmt"
+	"k8s.io/client-go/rest"
+	"os"
+	"path/filepath"
+	"context"
+
+	"github.com/pkg/errors"
+	"github.com/ghodss/yaml"
+	//appsv1 "k8s.io/api/apps/v1"
+	v1 "k8s.io/api/core/v1"
+	//v1net "k8s.io/api/networking/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	//"k8s.io/client-go/kubernetes/scheme"
+	"k8s.io/client-go/tools/clientcmd"
+	//"k8s.io/client-go/tools/remotecommand"
+)
+
+type Kubernetes struct {
+	podCache  map[string][]v1.Pod
+	ClientSet *kubernetes.Clientset
+}
+
+func NewKubernetes() (*Kubernetes, error) {
+	clientSet, err := Client()
+	if err != nil {
+		return nil, errors.WithMessagef(err, "unable to instantiate kube client")
+	}
+	return &Kubernetes{
+		podCache:  map[string][]v1.Pod{},
+		ClientSet: clientSet,
+	}, nil
+}
+
+func Client() (*kubernetes.Clientset, error) {
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		kubeconfig := filepath.Join(
+			os.Getenv("KUBECONFIG"), //TODO: Update path based on where this file is run from
+		)
+		config, err = clientcmd.BuildConfigFromFlags("", kubeconfig)
+		if err != nil {
+			return nil, errors.WithMessagef(err, "unable to build config from flags, check that your KUBECONFIG file is correct !")
+		}
+	}
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		return nil, errors.WithMessagef(err, "unable to instantiate clientset")
+	}
+	return clientset, nil
+}
+
+func (k *Kubernetes) ListK8sPolicies() error {
+	l, err := k.ClientSet.NetworkingV1().NetworkPolicies("").List(context.TODO(), metav1.ListOptions{})
+	if err != nil {
+		return errors.Wrapf(err, "unable to list network policies")
+	}
+	for _, np := range l.Items {
+		y, err := yaml.JSONToYAML([]byte(np.Annotations["kubectl.kubernetes.io/last-applied-configuration"]))
+		if err != nil {
+			return errors.Wrapf(err, "unable to convert network policy object")
+		}
+		fmt.Println(string(y))
+	}
+	return nil
+}
+
+func main() {
+	k8s, err := NewKubernetes()
+	if err != nil {
+		fmt.Printf("something went wrong")
+	}
+	k8s.ListK8sPolicies()
+}
