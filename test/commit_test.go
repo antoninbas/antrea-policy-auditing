e2ef9cb0ac4d0c2d7f3d4295adebddedff24c8de
Dhruv Jain <jaindh@jaindh-a01.vmware.com>
2021-06-15 11:55:54 -0700

    Cleaner code.

--- test/commit_test.go
+++ test/commit_test.go
@@ -1,33 +1,35 @@
 package main
 
 import (
+    "fmt"
     "testing"
     "io/ioutil"
     "encoding/json"
 
-    "antrea-audit/git-manager/commit"
+    "antrea-audit/git-manager/gitops"
 
     auditv1 "k8s.io/apiserver/pkg/apis/audit/v1"
 )
 
 func TestEventListToCommit(t *testing.T) {
-    jsonStr, _ := ioutil.ReadFile("/files/audit-log.txt")
+    jsonStr, err := ioutil.ReadFile("./files/audit-log.txt")
     eventList := auditv1.EventList{}
-    err := json.Unmarshal(jsonStr, &eventList)
+    err = json.Unmarshal(jsonStr, &eventList)
 
-    err = commit.EventListToCommit(eventList)
+    err = gitops.EventListToCommit(eventList)
 
     if err != nil {
+        fmt.Println(err)
         t.Errorf("Error (TestEventListToCommit): should not return error for correct event list")
     }
 }
 
 func TestModifyFiles(t *testing.T) {
-    jsonStr, _ := ioutil.ReadFile("/files/audit-log.txt")
+    jsonStr, _ := ioutil.ReadFile("./files/audit-log.txt")
     eventList := auditv1.EventList{}
     err := json.Unmarshal(jsonStr, &eventList)
 
-    err = commit.ModifyFiles(eventList)
+    err = gitops.ModifyFiles(eventList)
 
     if err != nil {
         t.Errorf("Error (ModifyFiles): should not return error for correct event list")
