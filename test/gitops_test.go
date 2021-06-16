406663f403953095463cb5ee8ff5b1c83d19511d
Dhruv Jain <jaindh@jaindh-a01.vmware.com>
2021-06-16 16:15:17 -0700

    Minor changes to AddAndCommit

--- test/gitops_test.go
+++ test/gitops_test.go
@@ -6,7 +6,12 @@ import (
     "io/ioutil"
 
     "antrea-audit/git-manager/gitops"
+
+    billy "github.com/go-git/go-billy/v5"
+	memory "github.com/go-git/go-git/v5/storage/memory"
 )
+
+var directory string
 
 func TestHandleEventList(t *testing.T) {
     jsonStr, err := ioutil.ReadFile("./files/audit-log.txt")
@@ -17,4 +22,9 @@ func TestHandleEventList(t *testing.T) {
         fmt.Println(err)
         t.Errorf("Error (TestHandleEventList): should not return error for correct event list")
     }
+}
+
+func SetupMemRepo(storer *memory.Storage, fs billy.Filesystem) (error) {
+    os.Mkdir(directory + "/network-policy-repository/", 0700)
+    r, err := git.Init(storer, fs)
 }
