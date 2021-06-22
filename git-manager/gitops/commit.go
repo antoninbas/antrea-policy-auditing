f95a2eff3a430d53c7252a875fdd6048fba07050
Stan Wong <swong394@gmail.com>
2021-06-22 15:36:21 -0400

    add specificity to commit messages

--- git-manager/gitops/commit.go
+++ git-manager/gitops/commit.go
@@ -16,10 +16,16 @@ import (
     billy "github.com/go-git/go-billy/v5"
 )
 
-var dirmap = map[string]string{
+var dirMap = map[string]string{
     "networkpoliciesnetworking.k8s.io": "k8s-policies",
     "networkpoliciescrd.antrea.io": "antrea-policies",
     "clusternetworkpoliciescrd.antrea.io": "antrea-cluster-policies",
+}
+
+var resourceMap = map[string]string{
+    "networkpoliciesnetworking.k8s.io": "K8s network policy ",
+    "networkpoliciescrd.antrea.io": "Antrea network policy ",
+    "clusternetworkpoliciescrd.antrea.io": "Antrea cluster network policy ",
 }
 
 func AddAndCommit(r *git.Repository, username string, email string, message string) (error) {
@@ -27,12 +33,10 @@ func AddAndCommit(r *git.Repository, username string, email string, message stri
     if err != nil {
         return err
     }
-
     _, err = w.Add(".")
     if err != nil {
         return err
     }
-
     _, err = w.Commit(message, &git.CommitOptions{
         Author: &object.Signature{
             Name: username,
@@ -47,15 +51,11 @@ func AddAndCommit(r *git.Repository, username string, email string, message stri
 }
 
 func GetRepoPath(dir string, event auditv1.Event) (string) {
-    return dir+"/"+dirmap[event.ObjectRef.Resource+event.ObjectRef.APIGroup]+"/"+event.ObjectRef.Namespace+"/"
+    return dir+"/"+dirMap[event.ObjectRef.Resource+event.ObjectRef.APIGroup]+"/"+event.ObjectRef.Namespace+"/"
 }
 
 func GetFileName(event auditv1.Event) (string) {
     return event.ObjectRef.Name+".yaml"
-}
-
-func EventToCommit(r *git.Repository, event auditv1.Event) (error) {
-    return AddAndCommit(r, event.User.Username, event.User.Username+event.User.UID+"@audit.antrea.io", "Network Policy Change for file: "+GetFileName(event))
 }
 
 func ModifyFile(dir string, event auditv1.Event) (error) {
@@ -109,32 +109,34 @@ func HandleEventList(dir string, jsonstring []byte) (error) {
         if event.Stage != "ResponseComplete" || event.ResponseStatus.Status == "Failure" {
             continue
         }
+        r, err := git.PlainOpen(dir)
+        if err != nil {
+            return err
+        }
+        user := event.User.Username
+        email := event.User.Username+event.User.UID+"@audit.antrea.io"
+        message := resourceMap[event.ObjectRef.Resource+event.ObjectRef.APIGroup]+event.ObjectRef.Name
         switch verb := event.Verb; verb {
         case "create":
             err = ModifyFile(dir, event)
             if err != nil {
                 return err
             }
+            AddAndCommit(r, user, email, "Created "+message)
         case "patch":
             err = ModifyFile(dir, event)
             if err != nil {
                 return err
             }
+            AddAndCommit(r, user, email, "Updated "+message)
         case "delete":
             err = EventToDelete(dir, event)
             if err != nil {
                 return err
             }
+            AddAndCommit(r, user, email, "Deleted "+message)
         default:
             continue
-        }
-        r, err := git.PlainOpen(dir)
-        if err != nil {
-            return err
-        }
-        err = EventToCommit(r, event)
-        if err != nil {
-            return err
         }
     }
 
@@ -154,26 +156,31 @@ func HandleEventListInMem(dir string, r *git.Repository, fs billy.Filesystem, js
         if event.Stage != "ResponseComplete" || event.ResponseStatus.Status == "Failure" {
             continue
         }
+        user := event.User.Username
+        email := event.User.Username+event.User.UID+"@audit.antrea.io"
+        message := resourceMap[event.ObjectRef.Resource+event.ObjectRef.APIGroup]+event.ObjectRef.Name
         switch verb := event.Verb; verb {
         case "create":
             err = ModifyFileInMem(dir, fs, event)
             if err != nil {
                 return err
             }
+            AddAndCommit(r, user, email, "Created "+message)
         case "patch":
             err = ModifyFileInMem(dir, fs, event)
             if err != nil {
                 return err
             }
+            AddAndCommit(r, user, email, "Updated "+message)
         case "delete":
             err = EventToDeleteInMem(dir, fs, event)
             if err != nil {
                 return err
             }
+            AddAndCommit(r, user, email, "Deleted "+message)
         default:
             continue
         }
-        err = EventToCommit(r, event)
     }
     return nil
 }
