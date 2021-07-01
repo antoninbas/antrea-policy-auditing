80b6719e54b221abee02edd183397a1ec989a48f
Stan Wong <swong394@gmail.com>
2021-07-01 13:34:17 -0400

    add delete tag function

--- git-manager/gitops/tagging.go
+++ git-manager/gitops/tagging.go
@@ -24,6 +24,14 @@ func TagCommit(r *git.Repository, commit_sha string, tag string, tagger *object.
 	return nil
 }
 
+func RemoveTag(r *git.Repository, tag string) error {
+	if err := r.DeleteTag(tag); err != nil {
+		klog.ErrorS(err, "Unable to delete tag")
+		return err
+	}
+	return nil
+}
+
 func setTag(r *git.Repository, commit_sha plumbing.Hash, tag string, tagger *object.Signature) error {
 	if tagExists(r, tag) {
 		klog.V(2).Infof("Unable to create tag: %s already exists", tag)
