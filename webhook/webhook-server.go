028c65383e8410e3db90a0c7880dc3bf229190e3
Stan Wong <swong394@gmail.com>
2021-07-13 14:16:23 -0400

    add response codes to webhook

--- webhook/webhook-server.go
+++ webhook/webhook-server.go
@@ -1,30 +1,32 @@
 package webhook
 
 import (
-    "io/ioutil"
-    "net/http"
+	"io/ioutil"
+	"net/http"
 
-    "antrea-audit/git-manager/gitops"
+	"antrea-audit/git-manager/gitops"
 
-    "k8s.io/klog/v2"
+	"k8s.io/klog/v2"
 )
 
 func ReceiveEvents(dir string, port string) error {
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        defer r.Body.Close()
-        body, err := ioutil.ReadAll(r.Body)
-        if err != nil {
-            klog.ErrorS(err, "unable to read audit body")
-        }
-        klog.V(3).Infof("Audit received: %s", string(body))
-        if err := gitops.HandleEventList(dir, body); err != nil {
-            klog.ErrorS(err, "unable to process audit event list")
-        }
-    })
-    klog.V(2).Infof("Audit webhook server started, listening on port %s", port)
-    if err := http.ListenAndServe(":"+string(port), nil); err != nil {
-	    klog.ErrorS(err, "Audit webhook service died")
-        return err
-    }
-    return nil
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		defer r.Body.Close()
+		body, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			klog.ErrorS(err, "unable to read audit body")
+			w.WriteHeader(http.StatusBadRequest)
+		}
+		klog.V(3).Infof("Audit received: %s", string(body))
+		if err := gitops.HandleEventList(dir, body); err != nil {
+			klog.ErrorS(err, "unable to process audit event list")
+			w.WriteHeader(http.StatusBadRequest)
+		}
+	})
+	klog.V(2).Infof("Audit webhook server started, listening on port %s", port)
+	if err := http.ListenAndServe(":"+string(port), nil); err != nil {
+		klog.ErrorS(err, "Audit webhook service died")
+		return err
+	}
+	return nil
 }
